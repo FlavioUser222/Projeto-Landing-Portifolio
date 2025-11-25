6de0217550526a8020dd0449877cf50539e94773
FlavioUser222 <flavinhoth12@gmail.com>
2025-11-25 17:10:20 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
-NODE_TLS_REJECT_UNAUTHORIZED='0'
-
 
 require('dotenv').config()
 const axios = require('axios')
 const express = require('express')
 const cors = require('cors')
+
+const https = require('https');
+
+const agent = new https.Agent({
+    rejectUnauthorized: false
+})
 
 
 const app = express()
@@ -15,20 +19,21 @@ app.use(cors({
 
 app.get('/projetos', async (req, res) => {
 
-        let response = await axios.get('https://api.github.com/users/FlavioUser222/repos', {
-            headers: {
-                Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-                Accept: "application/vnd.github+json"
-            }
-        })
+    let response = await axios.get('https://api.github.com/users/FlavioUser222/repos', {
+        headers: {
+            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+            Accept: "application/vnd.github+json"
+        },
+        httpsAgent: agent
+    })
 
-        const repos = response.data.map(repo => ({
-            nome: repo.name,
-            url: repo.html_url,
-            descricao: repo.description
-        }))
+    const repos = response.data.map(repo => ({
+        nome: repo.name,
+        url: repo.html_url,
+        descricao: repo.description
+    }))
 
-        res.json(repos)
+    res.json(repos)
 
 
 })
